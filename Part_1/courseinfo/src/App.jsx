fd3d3f5cdb975769062e5e0ce64bed51b58795b7
kraxios00 <kraxios00@gmail.com>
2025-04-21 12:00:46 -0600

    exercises 1.3 - 1.5

--- Part_1/courseinfo/src/App.jsx
+++ Part_1/courseinfo/src/App.jsx
@@ -3,29 +3,32 @@ import Content from "./components/Content/Content";
 import Total from "./components/Total/Total";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header title={course}/>
+      <Header title={course.name}/>
       <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        parts={course.parts}
       />
-      <Total 
-        exercises1={exercises1} 
-        exercises2={exercises2} 
-        exercises3={exercises3}
+      <Total
+        parts={course.parts} 
       />
     </>
   )
--- Part_1/courseinfo/src/components/Content/Content.jsx
+++ Part_1/courseinfo/src/components/Content/Content.jsx
@@ -1,11 +1,11 @@
 import Part from "../Part/Part"
 
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+const Content = ({parts}) => {
     return (
         <>
-            <Part name={part1} total={exercises1}/>
-            <Part name={part2} total={exercises2}/>
-            <Part name={part3} total={exercises3}/>
+            <Part name={parts[0].name} total={parts[0].exercises}/>
+            <Part name={parts[1].name} total={parts[1].exercises}/>
+            <Part name={parts[2].name} total={parts[2].exercises}/>
 
         </>
     )

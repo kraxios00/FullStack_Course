f5133480c2188f333895a7e879d7701bb5485b26
kraxios00 <kraxios00@gmail.com>
2025-04-24 22:09:47 -0600

    Exercises 2.16 - 2.17

--- Part_2/phone_book/src/App.jsx
+++ Part_2/phone_book/src/App.jsx
@@ -4,11 +4,19 @@ import Form from './components/Form'
 import Numbers from './components/Numbers'
 import Input from './components/Input'
 
+const TYPE_CONSTANTS = {
+  ADD: 'add',
+  UPDATE: 'update',
+  DELETE: 'delete',
+  ERROR: 'delete'
+}
+
 const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [persons, setPersons] = useState([])
+  const [message, setMessage] = useState({type: null, text: null})
 
   useEffect(() => {
         contactService.getAllContacts().then(res => setPersons(res.data))
@@ -17,19 +25,65 @@ const App = () => {
   const addPerson = (newContact) => {
       contactService.createContact(newContact).then(res => {
           setPersons(persons.concat(res.data))
+          setMessage({
+            ...message,
+              type: TYPE_CONSTANTS.ADD,
+              text: `Added ${res.data.name}`
+          })
+          setTimeout(() => {
+              setMessage({
+                ...message,
+                  text: null
+              })
+          }, 5000)
       })
   }
 
   const updatePerson = (id, updatedContact) => {
       contactService.updateContact(id, updatedContact).then(res => {
           setPersons(persons.map(person => person.id !== id ? person : res.data))
-      })
+          setMessage({
+            ...message,
+              type: TYPE_CONSTANTS.UPDATE,
+              text: `Updated ${res.data.name}`
+          })
+          setTimeout(() => {
+              setMessage({
+                ...message,
+                  text: null
+              })
+          }, 5000)
+      }).catch(error => {
+        console.error(error)
+        setMessage({
+          ...message,
+            type: TYPE_CONSTANTS.ERROR,
+            text: `Information of ${updatedContact.name} has already been removed from server`
+        })
+        setTimeout(() => {
+            setMessage({
+              ...message,
+                text: null
+            })
+        }, 5000)
+    })
   }
 
   const deletePerson = (id) => {
       contactService.deleteContact(id).then(() => {
           const newContactList = persons.filter(person => person.id !== id)
           setPersons(newContactList)
+          setMessage({
+            ...message,
+              type: TYPE_CONSTANTS.DELETE,
+              text: `Deleted ${persons.find(person => person.id === id).name}`
+          })
+          setTimeout(() => {
+              setMessage({
+                ...message,
+                  text: null
+              })
+          }, 5000)
       })
   }
 
@@ -70,6 +124,7 @@ const App = () => {
   return (
     <>
       <Input newValue={newFilter} setNewValue={setNewFilter} text="filter shown with"/>
+      {message.text && <p className={`message message_${message.type}`}>{message.text}</p>}
       <Form 
       newName={newName} 
       setNewName={handleContactChange} 

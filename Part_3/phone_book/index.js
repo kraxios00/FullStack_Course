a1c42b4ac169ea0c6b1390f313bab2329b5bb585
kraxios00 <kraxios00@gmail.com>
2025-05-01 16:32:27 -0600

    Exercises 3.7 - 3.8

--- Part_3/phone_book/index.js
+++ Part_3/phone_book/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const userRoutes = require('./src/routes/userRoutes.js')
+var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', req => JSON.stringify(req.body))
+  
+app.use(morgan(':method :url :status :response-time ms - body=:body'))
 app.use('/api', userRoutes)
 
 const PORT = process.env.PORT || 3000
